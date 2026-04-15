c77604c5f0d9c4e9dd08d929ef9333eb661455ca
Andrei S.T. <marioandreitesta@outlook.com.br>
2026-04-15 19:26:21 -0300

    exercicio(#013): reajuste salarial

---
+++ cursos/curso-em-video/mundo-1/exercicios/exercicio_#013.py
@@ -0,0 +1,9 @@
+# Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento. 
+
+print("Programa de Reajuste Salarial!")
+
+salario_atual = float(input("Digite o seu salário atual: "))
+
+print("Reajustando seu salário...")
+
+reajuste_salario = salario_atual * 1.15
