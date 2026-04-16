78927ab59c4cba2daeaee1e0073c81dd022f5e40
Andrei S.T. <marioandreitesta@outlook.com.br>
2026-04-15 22:00:25 -0300

    fix(exercicio_#009): adicionado código ausente

--- cursos/curso-em-video/mundo-1/exercicios/exercicio_#009.py
+++ cursos/curso-em-video/mundo-1/exercicios/exercicio_#009.py
@@ -1,1 +1,16 @@
-# 
+# Crie um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
+
+print("Gerador de Tabuada")
+num = int(input("Digite um número para ver a sua tabuada: "))
+print("-" * 12)
+print(f"{num} x 1 = {num * 1}")
+print(f"{num} x 2 = {num * 2}")
+print(f"{num} x 3 = {num * 3}")
+print(f"{num} x 4 = {num * 4}")
+print(f"{num} x 5 = {num * 5}")
+print(f"{num} x 6 = {num * 6}")
+print(f"{num} x 7 = {num * 7}")
+print(f"{num} x 8 = {num * 8}")
+print(f"{num} x 9 = {num * 9}")
+print(f"{num} x 10 = {num * 10}")
+print("-" * 12)
