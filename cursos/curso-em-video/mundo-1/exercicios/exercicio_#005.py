93a63f2a7c1088bddf29c204c4386dc945354e0f
Andrei S.T. <marioandreitesta@outlook.com.br>
2026-04-27 13:13:51 -0300

    refactor(exercicio_#005): adiciona cores ANSI

--- cursos/curso-em-video/mundo-1/exercicios/exercicio_#005.py
+++ cursos/curso-em-video/mundo-1/exercicios/exercicio_#005.py
@@ -1,11 +1,13 @@
 # Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e antecessor. 
 
-print("Escolha um número para saber seu sucessor e antecessor!")
+from cores import VERMELHO_BOLD, VERDE_BOLD, AMARELO_UNDERLINE, LIMPAR
 
-numero = int(input("Digite um número: "))
+print(f"Escolha um número para saber seu {VERDE_BOLD}sucessor{LIMPAR} e {VERMELHO_BOLD}antecessor!{LIMPAR}")
+
+numero = int(input(f"{AMARELO_UNDERLINE}Digite um número:{LIMPAR} "))
 
 sucessor = numero + 1
 antecessor = numero - 1
 
-print(f"Sucessor: {sucessor}")
-print(f"Antecessor: {antecessor}")
+print(f"Sucessor: {VERDE_BOLD}{sucessor}{LIMPAR}")
+print(f"Antecessor: {VERMELHO_BOLD}{antecessor}{LIMPAR}")
