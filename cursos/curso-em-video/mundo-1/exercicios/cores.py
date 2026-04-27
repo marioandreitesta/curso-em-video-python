aa1591ef32ba220ef0a82fc531f4185da20cdd51
Andrei S.T. <marioandreitesta@outlook.com.br>
2026-04-27 12:39:57 -0300

    docs(exercicios): adiciona cores.py

---
+++ cursos/curso-em-video/mundo-1/exercicios/cores.py
@@ -0,0 +1,7 @@
+# Cores para o último exercício do curso. 
+
+LIMPAR = "\033[m"
+PRETO_BACKGROUND = "\033[7;30;40m"
+VERDE_BOLD = "\033[1;32m"
+VERMELHO_BOLD = "\033[1;31m"
+AZUL_BOLD = "\033[1;34m"
