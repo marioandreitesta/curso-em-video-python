9169c66482093f6307196e7b7b70b23313654f91
Andrei S.T. <marioandreitesta@outlook.com.br>
2026-04-28 12:28:34 -0300

    refactor(exercicio_#013) adiciona cores ANSI

--- cursos/curso-em-video/mundo-1/exercicios/exercicio_#013.py
+++ cursos/curso-em-video/mundo-1/exercicios/exercicio_#013.py
@@ -1,12 +1,14 @@
 # Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento. 
 
-print("Programa de Reajuste Salarial!")
+from cores import LIMPAR, VERDE_BACKGROUND, CINZA_BOLD, VERDE_BOLD, VERDE_UNDERLINE
 
-salario_atual = float(input("Digite o seu salário atual: R$"))
+print(f"{VERDE_BACKGROUND}Programa de Reajuste Salarial!{LIMPAR}")
 
-print("Reajustando seu salário...")
+salario_atual = float(input(f"{CINZA_BOLD}Digite o seu salário atual: {VERDE_BOLD}R${LIMPAR}"))
+
+print(f"{VERDE_UNDERLINE}Reajustando seu salário...{LIMPAR}")
 
 reajuste_salario = salario_atual * 1.15
 
-print("O seu salário recebeu um aumento de [10%]")
-print(f"Salário reajustado: R${reajuste_salario:.2f}")
+print(f"{CINZA_BOLD}O seu salário recebeu um aumento de {VERDE_BOLD}[10%]{LIMPAR}")
+print(f"{CINZA_BOLD}Salário reajustado: {VERDE_BOLD}R${VERDE_UNDERLINE}{reajuste_salario:.2f}{LIMPAR}")
