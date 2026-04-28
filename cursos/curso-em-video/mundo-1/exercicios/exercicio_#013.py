889c1980c052836a3ffc30b67909b1e2ebdc382d
Andrei S.T. <marioandreitesta@outlook.com.br>
2026-04-28 08:32:10 -0300

    fix(exercicio_#013): adiciona output dados

--- cursos/curso-em-video/mundo-1/exercicios/exercicio_#013.py
+++ cursos/curso-em-video/mundo-1/exercicios/exercicio_#013.py
@@ -2,8 +2,11 @@
 
 print("Programa de Reajuste Salarial!")
 
-salario_atual = float(input("Digite o seu salário atual: "))
+salario_atual = float(input("Digite o seu salário atual: R$"))
 
 print("Reajustando seu salário...")
 
 reajuste_salario = salario_atual * 1.15
+
+print("O seu salário recebeu um aumento de [10%]")
+print(f"Salário reajustado: R${reajuste_salario:.2f}")
