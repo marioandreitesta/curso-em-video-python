3551d8e92bb4359d06a86d8958882be8e2228b7a
Andrei S.T. <marioandreitesta@outlook.com.br>
2026-04-28 15:56:00 -0300

    refactor(exercicio_#017): adiciona cores ANSI

--- cursos/curso-em-video/mundo-1/exercicios/exercicio_#017.py
+++ cursos/curso-em-video/mundo-1/exercicios/exercicio_#017.py
@@ -1,18 +1,21 @@
 # Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo.
 # Calcule e mostre o comprimento da hipotenusa. 
-DIV = 55
 
-print("-"*DIV)
-print("CALCULADORA DE HIPOTENUSA".center(DIV))
-print("-"*DIV)
+from cores import LIMPAR, ROXO_BOLD, AMARELO_BOLD, CINZA_BOLD
 
-cateto_oposto = float(input("Digite o comprimento do cateto oposto: "))
-cateto_adjacente = float(input("Digite o comprimento do cateto adjacente: "))
+DIV = 40
+
+print(f"{ROXO_BOLD}-{LIMPAR}"*DIV)
+print(f"{CINZA_BOLD}CALCULADORA DE HIPOTENUSA{LIMPAR}".center(DIV))
+print(f"{ROXO_BOLD}-{LIMPAR}"*DIV)
+
+cateto_oposto = float(input(f"{CINZA_BOLD}Digite o comprimento do cateto oposto: {AMARELO_BOLD}"))
+cateto_adjacente = float(input(f"{CINZA_BOLD}Digite o comprimento do cateto adjacente: {AMARELO_BOLD}"))
 soma_catetos  = (cateto_oposto ** 2) + (cateto_adjacente ** 2)
 hipotenusa = soma_catetos ** 0.5
 
-print("-"*DIV)
-print("Processando...".center(DIV))
-print("-"*DIV)
+print(f"{LIMPAR}{ROXO_BOLD}-{LIMPAR}"*DIV)
+print(f"{ROXO_BOLD}Processando...{LIMPAR}".center(DIV))
+print(f"{ROXO_BOLD}-{LIMPAR}"*DIV)
 
-print(f"O comprimento da hipotenusa é: {hipotenusa:.2f}")
+print(f"{CINZA_BOLD}O comprimento da hipotenusa é: {ROXO_BOLD}{hipotenusa:.2f}{LIMPAR}")
