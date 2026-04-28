dac7b020a989a88a4a89f43e32e2830753babfd3
Andrei S.T. <marioandreitesta@outlook.com.br>
2026-04-27 21:27:50 -0300

    refactor(exercicio_#008): adiciona cores ANSI

--- cursos/curso-em-video/mundo-1/exercicios/exercicio_#008.py
+++ cursos/curso-em-video/mundo-1/exercicios/exercicio_#008.py
@@ -1,12 +1,14 @@
 # Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros. 
 
-print("Conversor de Metros!")
+from cores import LIMPAR, CINZA_BOLD, VERDE_BOLD, AMARELO_UNDERLINE
 
-metros = float(input("Digite a metragem que deseja converter: "))
+print(f"{AMARELO_UNDERLINE}Conversor de Metros!{LIMPAR}")
+
+metros = float(input(f"{CINZA_BOLD}Digite a metragem que deseja converter [m]: {LIMPAR}"))
 
 CENTIMETROS = metros * 100
 MILIMETROS = metros * 1000
 
-print(f"{metros:.0f} metro(s) em:")
-print(f"Centímetros: {CENTIMETROS:.0f}cm")
-print(f"Milímetros: {CENTIMETROS:.0f}mm")
+print(f"{AMARELO_UNDERLINE}{metros:.0f} metro(s) em:{LIMPAR}")
+print(f"{CINZA_BOLD}Centímetros: {VERDE_BOLD}{CENTIMETROS:.0f}{LIMPAR}cm")
+print(f"{CINZA_BOLD}Milímetros: {VERDE_BOLD}{CENTIMETROS:.0f}{LIMPAR}mm")
