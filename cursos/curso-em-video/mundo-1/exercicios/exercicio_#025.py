f18d237ac0d216cd1dabccb131bb31636ca3f5af
Andrei S.T. <marioandreitesta@outlook.com.br>
2026-04-28 19:01:40 -0300

    refactor(exercicio_#025): adiciona cores ANSI

--- cursos/curso-em-video/mundo-1/exercicios/exercicio_#025.py
+++ cursos/curso-em-video/mundo-1/exercicios/exercicio_#025.py
@@ -1,11 +1,13 @@
 # Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
 
-nome = input('Digite seu nome completo: ').strip()
+from cores import LIMPAR, VERMELHO_BOLD, PRETO_BACKGROUND, VERDE_BOLD, CINZA_BOLD
+
+nome = input(f'{PRETO_BACKGROUND}Digite seu nome completo: {LIMPAR}').strip()
 
 nome_formatado = nome.upper()
-nome_verificador = 'SILVA'
+nome_verificador = f'{LIMPAR}SILVA'
 
 if nome_verificador in nome_formatado:
-    print('Seu nome tem a palavra "Silva"')
+    print(f'{CINZA_BOLD}Seu nome tem a palavra "{VERDE_BOLD}Silva{LIMPAR}"')
 else:
-    print('Seu nome não tem "Silva"')
+        print(f'{CINZA_BOLD}Seu nome não tem "{VERMELHO_BOLD}Silva{LIMPAR}"')
