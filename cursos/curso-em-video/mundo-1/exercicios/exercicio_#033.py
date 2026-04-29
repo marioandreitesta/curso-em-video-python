92b24562e9d0daea3072cdc79cb78ec76fec2824
Andrei S.T. <marioandreitesta@outlook.com.br>
2026-04-29 20:14:22 -0300

    refactor(exercicio_#033): adiciona cores ANSI

--- cursos/curso-em-video/mundo-1/exercicios/exercicio_#033.py
+++ cursos/curso-em-video/mundo-1/exercicios/exercicio_#033.py
@@ -1,32 +1,34 @@
 # Faça um programa que leia três números e mostre qual é o maior e qual é o menor. 
 
+import cores
+
 def pedir_numeros():
 
     while True:
-        entrada = input("\n Digite três números separados por espaço: ")
+        entrada = input(f"\n{cores.CINZA_BOLD}Digite {cores.ROXO_BOLD}três{cores.CINZA_BOLD} números separados por espaço: {cores.LIMPAR}")
         lista_numeros = entrada.split()
 
         if len(lista_numeros) != 3:
-            print(f"Erro: Você digitou {len(lista_numeros)} valor(es). É preciso três!")
+            print(f"{cores.VERMELHO_BOLD}Erro:{cores.CINZA_BOLD} Você digitou {cores.VERMELHO_BOLD}{len(lista_numeros)} {cores.CINZA_BOLD}valor(es). É preciso {cores.VERDE_BOLD}três!{cores.LIMPAR}")
             continue
 
         try:
             numeros = list(map(float, lista_numeros))
             return numeros
         except ValueError:
-            print("Oops! Certifique-se de digitar apenas números. Ex: 10 4.5 15")
+            print(f"{cores.VERMELHO_BOLD}Error:{cores.CINZA_BOLD} Certifique-se de digitar apenas números. Ex: {cores.VERDE_BOLD}10 4.5 15{cores.LIMPAR}")
 
 def exibir_resultados():
 
     menor = min(numeros)
     maior = max(numeros)
 
-    print("\n" + "=" * 38)
-    print(f"Números digitados: {numeros}")
-    print(f"Menor número: {menor}")
-    print(f"Maior número: {maior}")
-    print("=" * 38)
+    print(f"\n{cores.CINZA_BOLD}{'=' * 38}")
+    print(f"{cores.CINZA_BOLD}Números digitados: {cores.ROXO_BOLD}{numeros}")
+    print(f"{cores.CINZA_BOLD}Menor número: {cores.ROXO_BOLD}{menor}")
+    print(f"{cores.CINZA_BOLD}Maior número: {cores.ROXO_BOLD}{maior}")
+    print(f"{cores.CINZA_BOLD}{'=' * 38}{cores.LIMPAR}")
 
-print("Detector de Menor e Maior!")
+print(f"{cores.ROXO_BACKGROUND}Detector de Menor e Maior!{cores.LIMPAR}")
 numeros = pedir_numeros()
 exibir_resultados()
