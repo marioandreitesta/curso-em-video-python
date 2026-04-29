8e96dcae0b3f67e5b37f9c1afa052bc598b05ed4
Andrei S.T. <marioandreitesta@outlook.com.br>
2026-04-29 18:46:00 -0300

    refactor(exercicio_#027): adiciona cores ANSI

--- cursos/curso-em-video/mundo-1/exercicios/exercicio_#027.py
+++ cursos/curso-em-video/mundo-1/exercicios/exercicio_#027.py
@@ -1,12 +1,14 @@
 # Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente. 
 
+from cores import AZUL_BOLD, CINZA_BOLD, LIMPAR
+
 DIV = 40
 
 print('' * DIV)
-nome_usuario = input('Digite seu nome completo: ').strip()
-print('' * DIV)
+nome_usuario = input(f'{AZUL_BOLD}Digite seu nome completo: {CINZA_BOLD}').strip()
+print(f'{LIMPAR}' * DIV)
 
 nome_dividido = nome_usuario.split()
 
-print(f'Seu primeiro nome é: {nome_dividido[0]}')
-print(f'Seu último nome é: {nome_dividido[-1]}')
+print(f'{CINZA_BOLD}Seu primeiro nome é: {AZUL_BOLD}{nome_dividido[0]}')
+print(f'{CINZA_BOLD}Seu último nome é: {AZUL_BOLD}{nome_dividido[-1]}{LIMPAR}')
