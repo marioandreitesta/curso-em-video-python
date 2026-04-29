92a4a736fbb50453c3d0129a99b19321ae39db2f
Andrei S.T. <marioandreitesta@outlook.com.br>
2026-04-29 20:07:47 -0300

    refactor(exercicio_#032): adiciona cores ANSI

--- cursos/curso-em-video/mundo-1/exercicios/exercicio_#032.py
+++ cursos/curso-em-video/mundo-1/exercicios/exercicio_#032.py
@@ -1,15 +1,18 @@
 # Faça um programa que leia um ano qualquer e mostre se ele é bissexto. 
+
+from cores import LIMPAR, CINZA_BOLD, PRETO_BACKGROUND, VERMELHO_BOLD, VERDE_BOLD
+
 DIV = 27
 
-print('VERIFICADOR DE ANO BISSEXTO')
-print('-' * DIV)
+print(f'{PRETO_BACKGROUND}VERIFICADOR DE ANO BISSEXTO{LIMPAR}')
+print(f'{CINZA_BOLD}-{LIMPAR}' * DIV)
 
 try:
-    ano = int(input('Digite o ano: '))
+    ano = int(input(f'{CINZA_BOLD}Digite o ano: '))
 
     if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
-        print(f'O ano {ano} é bissexto!')
+            print(f'{CINZA_BOLD}O ano {VERDE_BOLD}{ano}{CINZA_BOLD} é bissexto!{LIMPAR}')
     else:
-        print('Esse ano não é bissexto.')
+        print(f'{CINZA_BOLD}O ano {VERMELHO_BOLD}{ano}{CINZA_BOLD} não é bissexto!{LIMPAR}')
 except ValueError:
-    print('Isso não é número, tente novamente.')
+    print(f'{VERMELHO_BOLD}Error{CINZA_BOLD}: isso não é um número!{LIMPAR}')
